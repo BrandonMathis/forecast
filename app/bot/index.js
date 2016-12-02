aba1bf051481299d0aa69d22e5d008f8d9c3b751
Brandon Mathis <bemathis@gmail.com>
2016-12-02 16:39:20 -0500

    Logs

--- app/bot/index.js
+++ app/bot/index.js
@@ -32,14 +32,10 @@ module.exports = function(bot) {
     }
   });
 
-  rtm.on(RTM_EVENTS.SUBTEAM_SELF_REMOVED, (message) => {
-    console.log(message);
-  });
-
   rtm.on(RTM_EVENTS.MESSAGE, (message) => {
-    console.log(message);
     if (message.text && message.text.match(RegExp(rtm.activeUserId))) {
       const location = message.text.replace(`<@${rtm.activeUserId}>`, '');
+      console.log(`🌤 Weather Requested for ${location}`);
       respondWithWeather(web, location, message.channel);
     }
   });
