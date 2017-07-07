58f1291e6097d4affd8bfceb350573e15174d629
Brandon Mathis <bemathis@gmail.com>
2017-07-07 14:33:05 -0400

    Added some color to logs

--- app/bot/index.js
+++ app/bot/index.js
@@ -9,6 +9,7 @@ const WebClient = require('@slack/client').WebClient;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const postMessage = require('./lib/slackWebClient').postMessage;
+const colors = require('colors');
 
 function respondWithWeather(bot, web, location, message) {
   const channel = message.channel;
@@ -122,7 +123,7 @@ module.exports = function(bot) {
       // @forecast <Location>
       //
       } else {
-        console.log(`🤖  Weather Requested for ${location}`);
+        console.log(`🤖  Team ${bot.teamName.cyan} requested weather for ${location}`);
         respondWithWeather(bot, web, location, message);
       }
     }
