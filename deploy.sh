da310518aa6858d1d1e7a8fa7640b30af46fb075
Brandon Mathis <bemathis@gmail.com>
2018-04-02 11:06:17 -0400

    Use pure lockfile

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 ssh root@forecastslackbot.com bash -c "'
 cd /var/www/forecast
 git pull origin master --force
-yarn install --frozen-lockfile
+yarn install --frozen-lockfile --pure-lockfile
 pm2 restart process.json --env production
 '"
