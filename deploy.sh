00bac62a188cefbeab7a95eac679556abdb555bc
Brandon Mathis <bemathis@gmail.com>
2018-04-02 11:03:47 -0400

    Frozen lockfile

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 ssh root@forecastslackbot.com bash -c "'
 cd /var/www/forecast
 git pull origin master --force
-yarn install
+yarn install --frozen-lockfile
 pm2 restart process.json --env production
 '"
