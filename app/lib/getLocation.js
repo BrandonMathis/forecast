19c34fbb15f23799d9b247ffcc6c0665a1b6621d
Brandon Mathis <bemathis@gmail.com>
2019-02-06 13:54:23 -0500

    Use google formatted location

--- app/lib/getLocation.js
+++ app/lib/getLocation.js
@@ -1,7 +1,6 @@
 const request = require('request');
 
-function getLocation(location) {
-  const givenLocation = location;
+function getLocation(givenLocation) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=${encodeURIComponent(location)}&key=${process.env.GOOGLE_MAPS_KEY}`;
   return new Promise((resolve, reject) => {
     request(url, (err, res, body) => {
@@ -10,8 +9,7 @@ function getLocation(location) {
       if (json.results.length === 0) { return reject(`Unable to find location ${givenLocation}`); }
       const lat = json.results[0].geometry.location.lat;
       const lng = json.results[0].geometry.location.lng;
-      const formattedAddress = json.results[0].formatted_address;
-      console.log(formattedAddress);
+      const location = json.results[0].formatted_address;
       resolve({ lat, lng, location });
     });
   });
