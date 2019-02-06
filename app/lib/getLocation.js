5048ad35d6d34ced03093f012dfbbe664ca942dd
Brandon Mathis <bemathis@gmail.com>
2019-02-06 13:41:26 -0500

    Fix getLocation

--- app/lib/getLocation.js
+++ app/lib/getLocation.js
@@ -2,15 +2,16 @@ const request = require('request');
 
 function getLocation(location) {
   const givenLocation = location;
-  this.url = `http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=${encodeURIComponent(location)}`;
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=${encodeURIComponent(location)}&key=${process.env.GOOGLE_MAPS_KEY}`;
   return new Promise((resolve, reject) => {
     request(url, (err, res, body) => {
+      if (err) { console.log(err); } // eslint-disable-line no-console
       const json = JSON.parse(body);
-      if (json.results.length === 0 ) { return reject(`Unable to find location ${givenLocation}`) };
+      if (json.results.length === 0) { return reject(`Unable to find location ${givenLocation}`); }
       const lat = json.results[0].geometry.location.lat;
       const lng = json.results[0].geometry.location.lng;
-      const location = json.results[0].formatted_address;
-      resolve({lat, lng, location});
+      const formattedAddress = json.results[0].formatted_address;
+      resolve({ lat, lng, formattedAddress });
     });
   });
 }
